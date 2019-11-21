9022ce434df2e6ffcb46e758e4960c33b6213860
Damien <damien@damienvalade.fr>
2019-11-21 13:40:47 +0100

    v0.4.3
    Add required file for js/css builder

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,72 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import Cropper from 'cropperjs/dist/cropper'
+import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router'
+import Routes from './routes'
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+import axios from 'axios'
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+Routing.setRoutingData(Routes)
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+var cropper
+var preview = document.getElementById('avatar')
+var file_input = document.getElementById('update_form_image')
+
+window.previewFile = function () {
+
+    let file = file_input.files[0]
+    let reader = new FileReader()
+
+    reader.addEventListener('load', function (event) {
+        preview.src = reader.result
+    }, false)
+
+    if (file) {
+        reader.readAsDataURL(file)
+    }
+
+    preview.addEventListener('load', function () {
+        if(cropper !== undefined){
+            cropper.destroy()
+        }
+        cropper = new Cropper(preview, {
+            aspectRatio: 1 / 1,
+            movable: false,
+            rotatable: false,
+            viewMode: 2
+        })
+
+    })
+}
+
+
+let form = document.getElementById('form_update')
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+    cropper.getCroppedCanvas({
+        maxHeight: 1000,
+        maxWidth: 1000
+    }).toBlob(function (blob) {
+        ajaxWithAxios(blob)
+    })
+})
+
+function ajaxWithAxios(blob) {
+    let url = Routing.generate('app_settings')
+    let data = new FormData(form)
+    data.append('file', blob)
+    axios({
+        method: 'POST',
+        url: url,
+        data: data,
+        headers: {'X-Requested-With' : 'XMLHttpRequest'}
+    })
+        .then((response) => {
+
+            if( response.data === 'redirect')
+            {
+                window.location.replace('/settings')
+            }
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+}
